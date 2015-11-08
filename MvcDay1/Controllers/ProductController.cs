1ab987bc6bdddaf4359e0f289e6122dae5f5c7c7
nobeadams <nobeadams@gmail.com>
2015-11-08 23:17:12 +0200

    First Commit

---
+++ MvcDay1/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDay1.Models;
+
+namespace MvcDay1.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+        public static List<Product> _products = new List<Product>()
+        {
+            new Product(){Id = 1, Name = "Nokia", Price = 120},
+            new Product(){Id = 2, Name = "Alcatel", Price = 450},
+            new Product(){Id = 3, Name = "Samsung", Price = 1200}
+        }; 
+        public ActionResult Index()
+        {
+            return View(_products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(int Id, string Name, double Price)
+        {
+            Product pro = new Product() {Id = Id, Name = Name, Price = Price};
+            _products.Add(pro);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MvcDay1/Controllers/StoreController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDay1.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public int Index(int x1, int x2)
+        {
+            return x1 + x2;
+        }
+        [Authorize]
+        public ActionResult MyName()
+        {
+            ViewData["Name1"] = "Abanoub Nageh";
+            ViewBag.Name = "Samy Reda";
+            return View();
+        }
+
+        public ActionResult MyWebsite()
+        {
+            return RedirectPermanent("http://www.facebook.com");
+        }
+
+        public ActionResult ToAnotherAction()
+        {
+            return RedirectToAction("MyName");
+        }
+
+        public ActionResult OpenFile()
+        {
+            return File("~/Scripts/jquery-1.7.1.js","javascript");
+        }
+
+        public ActionResult ReturnJson()
+        {
+            object fullName = new {FName = "Abanoub", LName = "nageh"};
+            return Json(fullName, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
